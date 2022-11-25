59ad9aeaafa38392ceb2c2d37ebdadae0a814f75
DashaMarchenko <you@example.com>
2022-11-25 20:08:46 +0300

    0

---
+++ src/core/Component/Component.js
@@ -0,0 +1,40 @@
+export class Component extends HTMLElement {
+  constructor() {
+    super();
+    this.state = {};
+    this.props = {};
+  }
+
+  setState(callback) {
+    this.state = callback(this.state);
+    this.innerHTML = this.render()
+  }
+
+  connectedCallback() {
+    this.innerHTML = this.render()
+    this.componentDidMount();
+    this.registerEvents();
+  }
+
+  disconnectedCallback() {
+    this.componentWillUnmount();
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.componentWillUpdate(name, oldValue, newValue);
+    this.getAttributeNames().forEach((name) => {
+      this.props[name] = this.getAttribute(name);
+    });
+  }
+
+  dispatch(type, props) {
+    this.dispatchEvent(new CustomEvent(type, { bubbles: true, detail: props }));
+  }
+
+  registerEvents() {}
+  componentDidMount() {}
+  componentWillUnmount() {}
+  componentWillUpdate() {}
+  componentWillUnmount() {}
+  render() {}
+}
